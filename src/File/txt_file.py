6170c11017878e24083e4346ccca7d6cc309c5cc
fantastic-4 <marcelo.rojas@jalasoft.com>
2013-07-12 14:16:25 -0400

    US29-Save the file dynamically without overwrite the file

--- src/File/txt_file.py
+++ src/File/txt_file.py
@@ -1,3 +1,4 @@
+import datetime
 from File.file import File
 from Main.grid import Grid
 
@@ -6,13 +7,13 @@ class TXT_File(File):
     def __init__(self,path , name):
         File.__init__(self, path, name)
         self.file=File(path,name)
-        self.path=path
-        self.name=name
         self.grid=Grid()
 
-    '''Function to read a file from txt file until found separator= ======'''
+
     def read_file(self, sep='\n'):
-        value=''
+        '''Function to read a file from txt file until found separator= ======
+        , and return the sudoku game as string'''
+        value=""
         fileO= self.open_file()
         file_string = fileO.read().strip().split(sep)
         for i in file_string:
@@ -20,32 +21,22 @@ class TXT_File(File):
                 value += i
             else:
                 if self.validate.validate_values(value):
+               #if self.grid.validate_values(value):     
                     return value
                 else: 
-                    print "the file is incorrect or more than 81"; 
+                    print "File is not correct or has more than 81 numbers" 
 
+    def write_file(self, values):
+        '''This method save the game into TXT called Solved_game_timeStamp.txt
+         file where the path will be defined in main class'''
+        self.file_name=self.timeStamped("Solved_game")
+        value_r="==================== \n"\
+        +"    Sudoku Solved:    \n"\
+        +"==================== \n"
+        value_r+=self.grid.display(values)
+        self.create_file(value_r)
 
-                
-    ''' this method is private and this only add enter to the string in order to get a list of 9 x 9 to print.'''
-    def __add_enter_in_line__(self, string):
-        text_to_be_written = ""
-        rows = "ABCDEFGHI"
-        columns = "123456789"
-        chars = 0
-        for row in rows:
-            for column in columns:
-                if chars == 9:
-                    text_to_be_written +="\n"
-                    chars=0 
-                text_to_be_written += string[row + column]
-                chars+=1
-        return text_to_be_written
-
-    '''this method save the file in named file inserted'''
-    def output_file(self, values):
-        #self.file_name="Solved_sudoku.txt"
-        value_return=""
-        value_return+=self.__add_enter_in_line__(values)+"\n\n"+self.grid.display(values)
-        self.create_file(value_return)
-
+    def timeStamped(self,fname, fmt="{fname}_%Y_%m_%d_%H_%M_%S.txt"):
+        '''Method to add the timestamp to a saved file '''
+        return datetime.datetime.now().strftime(fmt).format(fname=fname)
 
