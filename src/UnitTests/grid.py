299b210ac3940d5990357c99b66c6f47140ba243
Marcelo Rojas <marcelo.rojas@jalasoft.com>
2013-07-16 11:36:32 -0400

    US25-CodeReview for all packages
    Refactoring Grid class, I created new Display class and remove the
    method display from grid, also I added instance of grid in Norvig

---
+++ src/UnitTests/grid.py
@@ -0,0 +1,46 @@
+class Grid:
+    
+    def __init__(self):
+        self.dictionary_of_grid = None
+        self.digits   = "123456789"
+        self.rows     = "ABCDEFGHI"
+        self.cols     = self.digits
+        self.squares  = self.__set_matrix__(self.rows, self.cols)
+        self.squares  = self.__set_matrix__(self.rows, self.cols)
+        self.unitlist = ([self.__set_matrix__(self.rows, c) for c in self.cols] +
+            [self.__set_matrix__(r, self.cols) for r in self.rows] +
+            [self.__set_matrix__(rs, cs) for rs in ("ABC","DEF","GHI")
+             for cs in ("123","456","789")])
+
+        self.units = dict((s, [u for u in self.unitlist if s in u])
+                     for s in self.squares)
+        self.peers = dict((s, set(sum(self.units[s],[]))-set([s]))
+                     for s in self.squares)
+    
+    def __set_matrix__(self, A, B):
+        '''__set_matrix__ product of elements in A and elements in B.'''
+        return [a+b for a in A for b in B]
+
+    def set_values(self,gridx):
+        '''Function to create the grid using a grid list'''       
+        chars = [c for c in gridx]
+        return dict(zip(self.squares, chars))
+
+    def validate_grid(self, grid_string):
+        '''Function to validate the string given, if the values,
+            size and order of values is correct,
+            then it will be converted into a dictionary to be solved'''
+        flag = self.validate_values(grid_string)
+        if(flag):
+            self.dictionary_of_grid = self.set_values(grid_string)
+            for row in self.rows:
+                for column in self.cols:
+                    if(not self.check_lines(self.dictionary_of_grid[row+column],
+                        row+column, self.dictionary_of_grid)
+                        and not self.check_square(self.dictionary_of_grid[row+column],
+                        row+column, self.dictionary_of_grid)):
+                        flag = False                        
+                        break
+                if(not flag): break
+        return flag
+        
---
+++ src/UnitTests/norvig.py
@@ -0,0 +1,75 @@
+"""this class is using some attributes from grid like:squares, cols, rows and this needs a value (not a grid) to print""" 
+from Solver.solve_algorithm import Solve_Algorithm
+import time
+from Parser.grid import Grid
+class Norvig (Solve_Algorithm):
+
+    def __init__(self):
+        Solve_Algorithm.__init__(self)
+        self.grid = Grid() 
+
+    def __grid_to_dict__(self,grid):
+        '''Convert grid to a dict of possible values, {square: digits}, or
+        return False if a contradiction is detected.'''
+        values = dict((s,self.grid.digits) for s in self.grid.squares)
+        for s,d in self.grid.set_values(grid).items():
+            if d in self.grid.digits and not self.__eliminate_other_value__(values, s, d):
+                return False
+        return values
+
+    def __eliminate_other_value__(self, values, s, d):
+        '''Eliminate all the other values (except d) from values[s] and propagate.
+        Return values, except return False if a contradiction is detected.'''
+        other_values = values[s].replace(d, '')
+        if all(self.__eliminate__(values, s, d2) for d2 in other_values):
+            return values
+        else:
+            return False
+        
+    def __eliminate__(self,values, s, d):
+        '''eliminate d from values[s]; propagate when values or places <= 2.
+        Return values, except return False if a contradiction is detected.'''
+        if d not in values[s]:
+            return values
+        values[s] = values[s].replace(d,'')
+        if len(values[s]) == 0:
+            return False
+        elif len(values[s]) == 1:
+            d2 = values[s]
+            if not all(self.__eliminate__(values, s2, d2) for s2 in self.grid.peers[s]):
+                return False
+        for u in self.grid.units[s]:
+            dplaces = [s for s in u if d in values[s]]
+            if len(dplaces) == 0:
+                return False
+            elif len(dplaces) == 1:
+                if not self.__eliminate_other_value__(values, dplaces[0], d):
+                    return False
+        return values
+    
+    def solve(self,gridA): 
+        '''verify if the grid is correct then call to search and transform the grid to dict'''
+        self.time_elapsed = time.clock()
+        result = self.__search__(self.__grid_to_dict__(gridA))
+        self.time_elapsed = time.clock() - self.time_elapsed
+        return result
+ 
+    def __search__(self,values):
+        '''Using depth-first to search and propagate, try all possible values.'''
+        if values is False:
+            return False
+        if all(len(values[s]) == 1 for s in self.grid.squares):
+            return values 
+        n,s = min((len(values[s]), s) for s 
+                  in self.grid.squares if len(values[s]) > 1)
+        return self.__return_some_value__(self.__search__
+                                          (self.__eliminate_other_value__
+                                           (values.copy(), s, d))
+                    for d in values[s])
+      
+    
+    def __return_some_value__(self,seq):
+        '''This method returns some element of seq that is true.'''
+        for e in seq:
+            if e: return e
+        return False
