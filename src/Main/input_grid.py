c7dc77ba864458c75816b5eedc1f744b79ef7811
Christian-Canipa <christian.canipa@jalasoft.com>
2013-07-16 19:47:12 -0400

    US25-CodeReview for all packages
    Refactor classes to read a grid from keyboard

--- src/Main/input_grid.py
+++ src/Main/input_grid.py
@@ -1,22 +1,29 @@
 from Parser.grid import Grid
-def input_function():
-    print("\n***** Input Sudoku Game *****")
+def input_grid():
+    '''
+    Function to input a Sudoku grid from keyboard.
+    '''
+    print("\n***** Input Sudoku Grid *****")
     print("\nInstructions:\n")
     print("1) Enter 81 numbers and press Enter key.")
     print("2) Use '0' or '.' to make reference to empty slots.")
     print("3) Press Enter key, before complete the 81 numbers, to fill the rest with empty slots.")
     print("4) Only Numbers are allowed, at any error you will be prompted to start again")
-    print("5) To exit this option press Escape key\n")
+    print("5) To exit this option enter 'q'\n")
     
     flag = False
     while not flag:
         input_text = raw_input("Start to enter numbers: ")
         if(input_text in "qQ"): break
-        input_text,flag = validate_text(input_text)
+        input_text,flag = __validate_text__(input_text)
     if(flag): print input_text
     
-'''This function could be moved to Validate class.'''
-def validate_text(text):
+def __validate_text__(text):
+    '''
+    Function to validate the grid and if it is valid and its size is not 81 characters,
+    then it will be completed wit '0'.
+    :param text: text with characters to be used on Sudoku grid.
+    '''
     while(len(text) < 81): text += "0"
     grid = Grid()
     flag = grid.validate_grid(text)
