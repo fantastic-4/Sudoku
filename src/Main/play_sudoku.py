116bd92fa13c322a2f7a982204186009e5ce4779
Christian-Canipa <christian.canipa@jalasoft.com>
2013-07-19 18:33:01 -0400

    Add new methods and also its test cases
    US38-Implement last good square in console
    US37-Implement Reset method in console
    US36-Implement Hint method in console
    US33-Undo function for Play sudoku

--- src/Main/play_sudoku.py
+++ src/Main/play_sudoku.py
@@ -10,9 +10,9 @@ class Play_sudoku:
         
         self.validator = Validator()
         self.dictionary = dictionary
+        self.initial_state = dictionary
         self.algorithm = algorithm
-        self.empties = []
-        self.__grid_empty_values()
+        self.empties = self.__grid_empty_values([])
         
         self.norvig = Norvig()
         self.brute = Brute()
@@ -20,9 +20,10 @@ class Play_sudoku:
         self.squares = [a+b for a in self.validator.rows for b in self.validator.digits]
         self.time_elapsed = 0
         self.start_time = time.clock()
-        
+        self.initial_time = "00:00"
         self.path = save_path
         self.file_name = ""
+        self.moves_played = []
     
     def __play_move(self, key, num):
         '''
@@ -37,15 +38,14 @@ class Play_sudoku:
             return True
         else: return False
     
-    def __grid_empty_values(self):
+    def __grid_empty_values(self,empty_grid):
         '''
         Function to select all the tuples on dictionary with value as Zero
         '''
-    
         for key,value in self.dictionary.items():
             if(value == "0" or value == "."):
-                self.empties.append((key,value))
-        self.empties = dict(self.empties)
+                empty_grid.append((key,value))
+        return dict(empty_grid)
     
     def __get_hint(self,key):
         '''
@@ -66,24 +66,31 @@ class Play_sudoku:
                 return False
         return True
     
-    def __load_game(self, dictionary):
+    def __reset_game(self):
         '''
         Function to load a game from a previous saved game.
         :param dictionary: Dictionary from saved game.
         '''
-        self.dictionary = dictionary
-        self.__grid_empty_values()
+        self.time_elapsed = 0
+        self.start_time = time.clock()
+        self.set_start_time(self.initial_time)
+        self.dictionary = self.initial_state
+        self.empties = self.__grid_empty_values([])
     
-    def __solve(self):
+    def __solve(self, dict_to_solve):
         '''
         Function to Solve the current grid using the default algorithm
         '''
         dict_algorithm = {"norvig": self.norvig.solve, "brute": self.brute.solve}
-        dict_solved = dict_algorithm.get(self.algorithm)(self.dictionary)
+        dict_solved = dict_algorithm.get(self.algorithm)(dict_to_solve)
         
         return (dict_solved)
         
-    def __get_time(self):
+    def __calculate_time(self,parsed_time):
+        minutes,seconds = parsed_time.split(":")
+        return ((int(minutes)*60) + int(seconds))
+        
+    def get_time(self):
         '''
         Function to retrieve the time elapsed so far
         '''
@@ -99,6 +106,7 @@ class Play_sudoku:
         Function to set the time where the clock will start to run.
         :param time: Time to start to run the clock.
         '''
+        self.initial_time = new_time
         minutes,seconds = new_time.split(":")
         self.time_elapsed = (int(minutes)*60) + int(seconds)
     
@@ -107,30 +115,100 @@ class Play_sudoku:
         Function to save current game with the current time elapsed.
         '''
         file_to_save = TXT_File(self.path,self.file_name)
-        file_to_save.save_game(self.dictionary, self.__get_time())
+        file_to_save.save_game(self.dictionary, self.get_time())
         return ("The grid was saved")
+    
+    def __hint(self):
+        '''
+        Function to get a hint with the minor number of possibilities.
+        '''
+        if(self.norvig.solve(self.dictionary) != False):
+            current_values = self.norvig.grid_to_dict(self.dictionary)
+            current_empties = self.__grid_empty_values([])
+            if(len(current_empties)>0):
+                len_poss = 10
+                for pos in current_values.keys():
+                    if (pos in current_empties.keys()
+                        and len(current_values[pos]) < len_poss):
+                        len_poss = len(current_values[pos])
+                        min_pos = pos
+                hint = self.__calculate_hint(current_values, len_poss, min_pos)
+                return ("Hint: " + min_pos + ":" + current_values[min_pos][hint])
+            else: return ("No hints to give.")
+        else: return ("ERROR, Current Grid has no Solution")
         
+    def __calculate_hint(self,current_values, len_poss, min_pos):
+        '''
+        Function to calculate the hint according to parameters given.
+        :param current_values: Current values in the grid.
+        :param len_poss: Minor length of possibilities 
+        :param min_pos: Position where the minor possibilities are.
+        '''
+        hint = 0
+        if(len_poss > 1):
+            len_poss -=1
+            aux = self.dictionary
+            aux[min_pos] = current_values[min_pos][len_poss]
+            while(self.__solve(aux) == False):
+                len_poss -= 1
+                aux = self.dictionary
+                aux[min_pos] = current_values[min_pos][len_poss]
+            hint = len_poss
+        else: hint = len_poss - 1
+        return (hint)
+            
+    def __undo(self):
+        '''
+        Function to undo moves played before.
+        '''
+        if(len(self.moves_played)>0):
+            key = self.moves_played.pop()
+            self.dictionary[key] = "0"
+            return ("Last move was: " + key)
+        else:
+            return("No moves were made.")
+       
+    def __last_good_square(self):
+        '''
+        Function to return to last solvable game.
+        '''
+        if(self.norvig.solve(self.dictionary) != False):
+            while(len(self.moves_played)>0):
+                if(not self.__verify_game()):
+                    self.__undo()
+                else: break
+            else: return ("Game returned to last good game.")
+        else: return ("ERROR, Current Grid has no Solution")
+         
     def play(self,command):
         '''
         Function to read a command given and perform a method
         according to that command.
         :param command: command to be executed.
         '''
-        message = ""
         if(":" in command):
             key,num = command.split(":")
-            if(num.upper() == "HINT"): message = "Hint for: " + key + " -> " + self.__get_hint(key)
-            elif(self.__play_move(key, num)): message = "Number: " + num + "was added to: " + key
+            if(num.upper() == "HINT"):
+                message = "Hint for: " + key + " -> " + self.__get_hint(key)
+            elif(self.__play_move(key, num)):
+                message = "Number: " + num + "was added to: " + key
             else: message = "ERROR, Wrong Square"
         else:
             if(command.upper() == "VERIFY"): 
-                if(self.__verify_game()): message = "Congratulations, the You solved the Grid!!"
-                else: message = "ERROR, The grid is not fulfilled as expected."
+                if(self.__verify_game()):
+                    message = "Congratulations, You solved the Grid!!"
+                else:
+                    message = "ERROR, The grid is not fulfilled as expected."
             elif(command.upper() == "SOLVE"):
-                if(self.__solve() != False): message = "The grid was solved"
+                if(self.__solve(self.dictionary) != False):
+                    message = "The grid was solved"
                 else: message = "Current grid cannot be solved."
-            elif(command.upper() == "TIME"): message = "Time elapsed: " + self.__get_time()
             elif(command.upper() == "SAVE"): message = self.__save()
+            elif(command.upper() == "HINT"): message = self.__hint()
+            elif(command.upper() == "RESET"): 
+                self.__reset_game()
+                message = "The game was reset to initial state."
+            elif(command.upper() == "UNDO"): message = self.__undo()
+            elif(command.upper() == "RETURN"): message = self.__last_good_square()
             else: message = "ERROR, command not valid."
-                
         return message
--- src/UnitTests/Test_play_sudoku.py
+++ src/UnitTests/Test_play_sudoku.py
@@ -1,26 +1,30 @@
 import unittest
 from Main.play_sudoku import Play_sudoku
-from Solver.brute import Brute
 import os
-from hmac import new
+import time
 
-class Test(unittest.TestCase):
+class TestPlaySudoku(unittest.TestCase):
 
     def setUp(self):
         
         self.algorithm = "norvig"
         
         self.dictA = {"I6": "0", "H9": "9", "I2": "0", "E8": "0", "H3": "0", "H7": "0", "I7": "3", "I4": "0", "H5": "0", "F9": "0", "G7": "5", "G6": "9", "G5": "0", "E1": "7", "G3": "2", "G2": "0", "G1": "0", "I1": "0", "C8": "0", "I3": "5", "E5": "0", "I5": "1", "C9": "0", "G9": "0", "G8": "0", "A1": "0", "A3": "3", "A2": "0", "A5": "2", "A4": "0", "A7": "6", "A6": "0", "C3": "1", "C2": "0", "C1": "0", "E6": "0", "C7": "4", "C6": "6", "C5": "0", "C4": "8", "I9": "0", "D8": "0", "I8": "0", "E4": "0", "D9": "0", "H8": "0", "F6": "8", "A9": "0", "G4": "6", "A8": "0", "E7": "0", "E3": "0", "F1": "0", "F2": "0", "F3": "6", "F4": "7", "F5": "0", "E2": "0", "F7": "2", "F8": "0", "D2": "0", "H1": "8", "H6": "3", "H2": "0", "H4": "2", "D3": "8", "B4": "3", "B5": "0", "B6": "5", "B7": "0", "E9": "8", "B1": "9", "B2": "0", "B3": "0", "D6": "2", "D7": "9", "D4": "1", "D5": "0", "B8": "0", "B9": "1", "D1": "0"}
+        self.expected_dictA = {"I6": "2", "H9": "9", "I2": "0", "E8": "0", "H3": "0", "H7": "0", "I7": "3", "I4": "0", "H5": "0", "F9": "0", "G7": "5", "G6": "9", "G5": "0", "E1": "7", "G3": "2", "G2": "0", "G1": "0", "I1": "0", "C8": "0", "I3": "5", "E5": "0", "I5": "1", "C9": "0", "G9": "0", "G8": "0", "A1": "0", "A3": "3", "A2": "0", "A5": "2", "A4": "0", "A7": "6", "A6": "0", "C3": "1", "C2": "0", "C1": "0", "E6": "0", "C7": "4", "C6": "6", "C5": "0", "C4": "8", "I9": "0", "D8": "0", "I8": "0", "E4": "0", "D9": "0", "H8": "0", "F6": "8", "A9": "0", "G4": "6", "A8": "0", "E7": "0", "E3": "0", "F1": "0", "F2": "0", "F3": "6", "F4": "7", "F5": "0", "E2": "0", "F7": "2", "F8": "0", "D2": "0", "H1": "8", "H6": "3", "H2": "0", "H4": "2", "D3": "8", "B4": "3", "B5": "0", "B6": "5", "B7": "0", "E9": "8", "B1": "9", "B2": "0", "B3": "0", "D6": "2", "D7": "9", "D4": "1", "D5": "0", "B8": "0", "B9": "1", "D1": "0"}
         self.dict_complete = {"B8": "2", "H1": "8", "C7": "4", "B3": "7", "D3": "8", "G9": "4", "G8": "1", "B9": "1", "A3": "3", "G7": "5", "G6": "9", "G5": "8", "G4": "6", "G3": "2", "G2": "7", "G1": "3", "B5": "4", "I1": "6", "I3": "5", "I2": "9", "I5": "1", "I4": "4", "I7": "3", "I6": "7", "A1": "4", "C9": "3", "C8": "9", "A5": "2", "E8": "3", "A7": "6", "A6": "1", "E5": "6", "C2": "5", "C1": "2", "E6": "4", "E1": "7", "A2": "8", "C5": "7", "A4": "9", "I9": "2", "B2": "6", "I8": "8", "H2": "1", "D9": "6", "F2": "3", "D5": "3", "C3": "1", "A9": "7", "C6": "6", "E4": "5", "B1": "9", "E7": "1", "F1": "1", "H8": "6", "H9": "9", "F4": "7", "F5": "9", "F6": "8", "F7": "2", "F8": "4", "H3": "4", "F3": "6", "H6": "3", "H7": "7", "H4": "2", "H5": "5", "B4": "3", "A8": "5", "B6": "5", "B7": "8", "E9": "8", "E3": "9", "D8": "7", "F9": "5", "D6": "2", "D7": "9", "D4": "1", "C4": "8", "D2": "4", "E2": "2", "D1": "5"}
+        self.dictB = {"I6": "0", "H9": "0", "I2": "0", "E8": "5", "H3": "0", "H7": "0", "I7": "0", "I4": "7", "H5": "3", "F9": "0", "G7": "0", "G6": "0", "G5": "0", "E1": "6", "G3": "1", "G2": "4", "G1": "0", "I1": "0", "C8": "0", "I3": "8", "E5": "0", "I5": "0", "C9": "0", "G9": "0", "G8": "0", "A1": "5", "A3": "0", "A2": "2", "A5": "0", "A4": "0", "A7": "0", "A6": "6", "C3": "0", "C2": "0", "C1": "3", "E6": "0", "C7": "0", "C6": "0", "C5": "0", "C4": "0", "I9": "0", "D8": "0", "I8": "0", "E4": "0", "D9": "0", "H8": "2", "F6": "0", "A9": "0", "G4": "8", "A8": "0", "E7": "0", "E3": "0", "F1": "0", "F2": "0", "F3": "0", "F4": "0", "F5": "0", "E2": "0", "F7": "0", "F8": "0", "D2": "0", "H1": "0", "H6": "0", "H2": "0", "H4": "0", "D3": "0", "B4": "0", "B5": "0", "B6": "0", "B7": "7", "E9": "0", "B1": "0", "B2": "0", "B3": "0", "D6": "0", "D7": "8", "D4": "4", "D5": "0", "B8": "0", "B9": "1", "D1": "0"}
+        
         self.wrong_dict = {"I6": "9", "H9": "9", "I2": "9", "E8": "9", "H3": "9", "H7": "9", "I7": "3", "I4": "0", "H5": "0", "F9": "0", "G7": "5", "G6": "9", "G5": "0", "E1": "7", "G3": "2", "G2": "0", "G1": "0", "I1": "0", "C8": "0", "I3": "5", "E5": "0", "I5": "1", "C9": "0", "G9": "0", "G8": "0", "A1": "0", "A3": "3", "A2": "0", "A5": "2", "A4": "0", "A7": "6", "A6": "0", "C3": "1", "C2": "0", "C1": "0", "E6": "0", "C7": "4", "C6": "6", "C5": "0", "C4": "8", "I9": "0", "D8": "0", "I8": "0", "E4": "0", "D9": "0", "H8": "0", "F6": "8", "A9": "0", "G4": "6", "A8": "0", "E7": "0", "E3": "0", "F1": "0", "F2": "0", "F3": "6", "F4": "7", "F5": "0", "E2": "0", "F7": "2", "F8": "0", "D2": "0", "H1": "8", "H6": "3", "H2": "0", "H4": "2", "D3": "8", "B4": "3", "B5": "0", "B6": "5", "B7": "0", "E9": "8", "B1": "9", "B2": "0", "B3": "0", "D6": "2", "D7": "9", "D4": "1", "D5": "0", "B8": "0", "B9": "1", "D1": "0"}
         self.move_to_play = "I6:2"
         self.expected_dictA = {"I6": "2", "H9": "9", "I2": "0", "E8": "0", "H3": "0", "H7": "0", "I7": "3", "I4": "0", "H5": "0", "F9": "0", "G7": "5", "G6": "9", "G5": "0", "E1": "7", "G3": "2", "G2": "0", "G1": "0", "I1": "0", "C8": "0", "I3": "5", "E5": "0", "I5": "1", "C9": "0", "G9": "0", "G8": "0", "A1": "0", "A3": "3", "A2": "0", "A5": "2", "A4": "0", "A7": "6", "A6": "0", "C3": "1", "C2": "0", "C1": "0", "E6": "0", "C7": "4", "C6": "6", "C5": "0", "C4": "8", "I9": "0", "D8": "0", "I8": "0", "E4": "0", "D9": "0", "H8": "0", "F6": "8", "A9": "0", "G4": "6", "A8": "0", "E7": "0", "E3": "0", "F1": "0", "F2": "0", "F3": "6", "F4": "7", "F5": "0", "E2": "0", "F7": "2", "F8": "0", "D2": "0", "H1": "8", "H6": "3", "H2": "0", "H4": "2", "D3": "8", "B4": "3", "B5": "0", "B6": "5", "B7": "0", "E9": "8", "B1": "9", "B2": "0", "B3": "0", "D6": "2", "D7": "9", "D4": "1", "D5": "0", "B8": "0", "B9": "1", "D1": "0"}
         
-        self.hint_command = "I6:HiNt"
-        self.expected_hint_message = "Hint for: I6 -> 7"
+        self.specific_hint_command = "I6:HiNt"
+        self.expected_specific_hint_message = "Hint for: I6 -> 7"
+        self.hint_command = "HiNt"
+        self.expected_hint_message = "Hint: H9:8"
         
         self.verify_command = "VeRiFy"
-        self.expected_verify_message = "Congratulations, the You solved the Grid!!"
+        self.expected_verify_message = "Congratulations, You solved the Grid!!"
         self.expected_wrong_verify_message = "ERROR, The grid is not fulfilled as expected."
         
         self.wrong_move_to_play = "Z6:20"
@@ -32,59 +36,65 @@ class Test(unittest.TestCase):
         
         self.time_command = "TiMe"
         self.time_elapsed = "1:30"
+        self.time_playing = 3
         
         self.save_command = "SavE"
         self.path = "c:\\sudoku\\save\\"
+        
+        self.reset_command = "ReSeT"
+        
+        self.undo_command = "UnDo"
+        self.last_good_square_command = "ReTuRn"
         
     def test_if_a_number_can_be_set_as_move_played(self):
         ps = Play_sudoku(self.dictA, self.algorithm, self.path)
         ps.play(self.move_to_play)
         self.assertDictEqual(ps.dictionary,self.expected_dictA)
-        
+           
     def test_if_a_wrong_move_returns_false(self):
         ps = Play_sudoku(self.dictA, self.algorithm, self.path)
         self.assertTrue(ps.play(self.wrong_move_to_play), self.expected_wrong_coordinate_message)
-        
-    def test_if_hint_is_displayed(self):
+           
+    def test_if_specific_hint_is_displayed(self):
         ps = Play_sudoku(self.dictA, self.algorithm, self.path)
-        m = ps.play(self.hint_command)
-        self.assertEquals(m,self.expected_hint_message)
-        
+        m = ps.play(self.specific_hint_command)
+        self.assertEquals(m,self.expected_specific_hint_message)
+           
     def test_if_verify_command_validates_current_grid(self):
         ps = Play_sudoku(self.dict_complete, self.algorithm, self.path)
         m = ps.play(self.verify_command)
         self.assertEquals(m,self.expected_verify_message)
-        
+           
     def test_if_verify_command_validates_wrong_current_grid(self):
         ps = Play_sudoku(self.dictA, self.algorithm, self.path)
         m = ps.play(self.verify_command)
         self.assertEquals(m,self.expected_wrong_verify_message)
-    
+       
     def test_if_current_grid_can_be_solved(self):
         ps = Play_sudoku(self.dictA, self.algorithm, self.path)
         m = ps.play(self.solve_command)
         self.assertEquals(m,self.expected_solve_message)
-        
+           
     def test_if_current_grid_cannot_be_solved(self):
         ps = Play_sudoku(self.wrong_dict, self.algorithm, self.path)
         m = ps.play(self.solve_command)
         self.assertEquals(m,self.expected_wrong_solve_message)
-        
+           
     def test_if_time_elapsed_can_be_retrieved(self):
         ps = Play_sudoku(self.dictA, self.algorithm, self.path)
-        m = ps.play(self.time_command)
+        m = ps.get_time()
         m = m.split(":")
-        time_in_seconds = (float(m[1])*60) + float(m[2])
+        time_in_seconds = (float(m[0])*60) + float(m[1])
         self.assertTrue(time_in_seconds > 0)
-        
+           
     def test_if_time_elapsed_can_be_retrieved_after_load_previous_time(self):
         ps = Play_sudoku(self.dictA, self.algorithm, self.path)
         ps.set_start_time(self.time_elapsed)
-        m = ps.play(self.time_command)
+        m = ps.get_time()
         m = m.split(":")
-        time_in_seconds = (float(m[1])*60) + float(m[2])
+        time_in_seconds = (float(m[0])*60) + float(m[1])
         self.assertTrue(time_in_seconds > 90)
-        
+           
     def test_if_current_grid_can_be_saved(self):
         ps = Play_sudoku(self.wrong_dict, self.algorithm, self.path)
         first_files = os.listdir(self.path)
@@ -95,7 +105,41 @@ class Test(unittest.TestCase):
         self.assertTrue((files_saved + 1) == new_files_saved)
         for f in current_files:
             if not f in first_files: os.remove(self.path + f)
+          
+    def test_if_hint_is_displayed(self):
+        ps = Play_sudoku(self.dictB, self.algorithm, self.path)
+        self.assertTrue(ps.play(self.hint_command),self.expected_hint_message)
+          
+    def test_if_reset_command_restart_the_grid(self):
+        ps = Play_sudoku(self.dictB, self.algorithm, self.path)
+        ps.play(self.move_to_play)
+        ps.play(self.reset_command)
+        self.assertTrue(ps.dictionary,self.dictB)
+         
+    def test_if_reset_command_restart_the_time(self):
+        ps = Play_sudoku(self.dictB, self.algorithm, self.path)
+        ps.play(self.move_to_play)
+        time.sleep(self.time_playing)
+        ps.play(self.reset_command)
+        m = ps.get_time()
+        m = m.split(":")
+        time_in_seconds = (float(m[0])*60) + float(m[1])
+        self.assertTrue(time_in_seconds < self.time_playing)
         
+    def test_if_undo_remove_last_move_played(self):
+        ps = Play_sudoku(self.dictA, self.algorithm, self.path)
+        ps.play(self.move_to_play)
+        ps.play(self.undo_command)
+        self.assertDictEqual(ps.dictionary,self.dictA)
+        
+    def test_if_game_can_be_returned_to_last_good_state(self):
+        ps = Play_sudoku(self.dictA, self.algorithm, self.path)
+        ps.play(self.move_to_play)
+        ps.play("I2:9")
+        ps.play("I8:9")
+        ps.play(self.last_good_square_command)
+        self.assertDictEqual(self.dictA,ps.dictionary)
+                
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
