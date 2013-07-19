c6f969c2688fc8094b231a24983044f67d3d28a6
Marcelo Rojas <marcelo.rojas@jalasoft.com>
2013-07-19 15:11:40 -0400

    US39-Modify XML adding number of saved files and save file path
    Adding config file that was changed

--- src/File/config_file.py
+++ src/File/config_file.py
@@ -1,6 +1,12 @@
+import os
 class Config_file:
 	"""doc string for Config_file"""
 	def __init__(self, path, name):
 		self.name = name
 		self.path = path
 	
+	def ensure_dir(self,f):
+		''' This method create new dir if this does not exit'''
+		d = os.path.dirname(f)
+		if not os.path.exists(d):
+			os.makedirs(d)
