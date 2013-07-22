9190df3d5b8d82d493534aea9d3d87ff22f9d786
Arnold <Arnold.Huanca@jalasoft.com>
2013-07-22 16:13:33 -0400

    US40-Create new Solve Sudoku Class instead menu
    Refactoring 'Menu' class and 'Solve console' class

--- src/Console/menu.py
+++ src/Console/menu.py
@@ -3,14 +3,12 @@ Sudoku
 '''
 import os, sys
 sys.path.append("../../src")
-from Main.sudoku_game import SudokuGame
 from Console.solve_console import SolveConsole
 from Console.configure_sudoku import ConfigureSudoku
 
 class Menu():
         
     def __init__(self):
-        self.Game_console = SudokuGame()
         self.solve = SolveConsole()
         self.configure_settings = ConfigureSudoku()
         self.options = {1: self.solve.solve, 2: self.play_game,\
@@ -18,25 +16,6 @@ class Menu():
     
     def play_game(self):
         print "this is not implemented yet"
-    
-    def enter_option(self, length,text,error="Incorrect input!. \
-    \nPlease enter a proper option."):
-        while True:
-            num = raw_input(text)
-            try:
-                num = int(num)
-                if (num <= length):
-                    return num
-                    break
-                else:
-                    self.error_message(error)
-                    pass
-            except ValueError:
-                self.error_message(error)
-                pass
-    def error_message(self,text):
-        print text
-    
     
     def main(self):
         while (True):
@@ -50,7 +29,7 @@ class Menu():
             print "3: Configure Settings"
             print "4: Exit"
             
-            num = self.enter_option(4, "\nPlease enter an option: ")
+            num = self.solve.enter_option(4, "\nPlease enter an option: ")
             
             if (num == 4): 
                 print "............Exist.............\n"
