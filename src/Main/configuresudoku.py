3dd72286ca0ae932ff2c370ae6a31ca38859fae5
Marcelo Rojas <marcelo.rojas@jalasoft.com>
2013-07-22 10:03:03 -0400

    US42-Create new Configure Sudoku Class instead menu
    Marcelo Rojas: New Class

---
+++ src/Main/configuresudoku.py
@@ -0,0 +1,76 @@
+from File.config_file_xml import Xml_file
+import os
+import Main.menu
+class ConfigureSudoku:
+    def __init__(self):
+        self.xmlfile=Xml_file()
+        
+    def configure_settings(self):
+        self.display_current_values()
+        num = self.submenu_modify_values()
+        tags = {1: "default_algorithm", 2: "solver_output_type", 3: "difficulty_level", 4:"save_game_number"}
+        
+        if (num == 5):
+            Main.menu.menu_ini()
+        else:
+            option = self.display_options(num)
+            if not (option == 'Back'):
+                self.xmlfile.set_xml_value(option,tags.get(num))
+                print ".........Value updated....."
+                os.system("cls")
+            self.configure_settings()
+
+    def display_options(self,num):
+        algoritms = ["1: Norvig","2: Brute", "3: Back" ]
+        outputTypes =  ["1: Display by console", "2: Export to file", "3: Back"]
+        levels =  ["1: Easy", "2: Medium", "3: Hard", "4: Back"]   
+        toSelect = {1:algoritms, 2: outputTypes, 3:levels}
+        if num==4:
+            value=self.enter_option(100, "\n Please enter new value: \n", "Incorrect input!. Please enter correct value")
+            option=value
+            return int(value)
+        else:
+            for option in toSelect.get(num):
+                print option
+            option = self.enter_option(4,"\n Please enter an option: \n")
+            return toSelect.get(num)[int(option)-1][3:]
+
+    def display_current_values(self):
+        print "\n ======================================" 
+        print "         Current Values "
+        print "======================================" 
+        print "Algorithm: ", self.xmlfile.get_xml_value("default_algorithm").upper()
+        print "Output type: ", self.xmlfile.get_xml_value("solver_output_type").upper()
+        print "level: ", self.xmlfile.get_xml_value("difficulty_level").upper()
+        print "Number of saved games: ", self.xmlfile.get_xml_value("save_game_number")
+        print "======================================"    
+        
+    def submenu_modify_values(self):   
+        print "Modify Values"
+        print "======================================"    
+              
+        print "1: Change algorithm"
+        print "2: Change output type"
+        print "3: Change level"
+        print "4: Change number of saved games"
+        print "5: Back"
+        return self.enter_option(5,"\n Please enter an option: \n")
+
+    def enter_option(self,len,text,error="Incorrect input!. Please enter a proper option."):
+        while True:
+            num= raw_input(text)
+            try:
+                num=int(num)
+                if (num <= len):
+                    return num
+                    break
+                else:
+                    self.errorMessage(error)
+                    pass
+            except ValueError:
+                self.errorMessage(error)
+                pass
+    def errorMessage(self,text):
+        print text
+
+ConfigureSudoku()
