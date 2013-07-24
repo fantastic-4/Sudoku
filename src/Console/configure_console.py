e9a6bdf5d5058ae62ed443645789bbeb1a86edc2
Marcelo Rojas <marcelo.rojas@jalasoft.com>
2013-07-24 13:22:28 -0400

    US44-Refactoring Code
    fixed some tabs in configure console class

--- src/Console/configure_console.py
+++ src/Console/configure_console.py
@@ -1,13 +1,7 @@
-'''
-Submenu to configure settings of Sudoku game 
-'''
 import os
 
-
+'''This class handle all configuration stuff'''
 class ConfigureConsole:
-    '''
-    This class handle all configuration stuff
-    '''
     def __init__(self, sudoku):
         self.settings = sudoku
         
@@ -32,21 +26,24 @@ class ConfigureConsole:
     def display_options(self, num):
         ''' Displays all option for configuration that will be changed'''
         algoritms = ["1: Norvig", "2: Brute", "3: Dlx", "4: Back" ]
-        output_types =  ["1: Display by console", "2: Export to file", "3: Back"]
+        output_types = ["1: Display by console", "2: Export to file", "3: Back"]
         levels =  ["1: Easy", "2: Medium", "3: Hard", "4: Back"]   
         to_select = {1: algoritms, 2: output_types, 3: levels}
         if num == 4:
-            value = self.enter_option(100, "\n Please enter new value: ", \
-                   "\nIncorrect input!. Please enter correct value")
+            value=self.enter_option(100, "\nPlease enter new value: ", \
+                   "\nIncorrect input!. \nPlease enter correct value")
             option = value
             return int(value)
         else:
+            print "\n======================================" 
+            print "         Options "
+            print "======================================" 
             for option in to_select.get(num):  
                 print option
             if num == 2:
-                option = self.enter_option(3, "\n Please enter an option****:\n")
+                option = self.enter_option(3, "\nPlease enter an option: ")
             else:
-                option = self.enter_option(4, "\n Please enter an option...:\n")
+                option = self.enter_option(4, "\nPlease enter an option: ")
             return to_select.get(num)[int(option)-1][3:]
 
     def display_current_values(self):
@@ -77,10 +74,10 @@ class ConfigureConsole:
         print "3: Change level"
         print "4: Change number of saved games"
         print "5: Back"
-        return self.enter_option(5,"\n Please enter an option: \n")
+        return self.enter_option(5,"\nPlease enter an option: ")
 
     def enter_option(self, length, text, \
-                     error="Incorrect input!. Please enter a proper option."):
+                     error="Incorrect input!. \nPlease enter a proper option."):
         ''' asks for an value from input and validate 
         if this option is correct, this use error_message function'''
         
