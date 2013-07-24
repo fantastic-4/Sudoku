77c8f8694b917b6a38821c62f2f37391df24bdef
Arnold <Arnold.Huanca@jalasoft.com>
2013-07-24 10:21:48 -0400

    US45-Integrate all classes into Main Class
    Applying PEP8 and PEP257

--- src/Console/solve_console.py
+++ src/Console/solve_console.py
@@ -1,9 +1,14 @@
+'''
+Submenu to solve a sudoku game
+'''
 import os
 
 class SolveConsole():
-    
+    '''
+    Solve a sudoku game from console
+    '''
     def __init__(self, sudoku):
-        self.Game_console = sudoku
+        self.game_console = sudoku
            
     def solve(self):
         '''
@@ -16,13 +21,16 @@ class SolveConsole():
         print "\n1: Enter the Sudoku by file"
         print "2: Enter the Sudoku by console"
         print "3: Back"
-        option = self.enter_option(3,"\n Please enter an option: ")
+        option = self.enter_option(3,"\nPlease enter an option: ")
         
-        options_solve = {1: self.solve_from_a_file, 2: self.solve_from_console,\
+        options_solve = {1: self.solve_from_a_file, 2: self.solve_from_console, \
                           3: self.menu_ini}
         options_solve.get(option)()
     
     def menu_ini(self):
+        '''
+        Back to the principal menu 
+        '''
         return
     
     def solve_from_a_file(self):
@@ -31,16 +39,15 @@ class SolveConsole():
         '''
          
         while True:
-            valuepath = raw_input("\nEnter the path where\
- the file is located: ")
-            head= os.path.split(valuepath)
-            newhead=head[0]+ head[1]
-            if head[1]=="":
-                newhead=head[0]
-            elif(head[1][-4:-3]=="."):
-                newhead=head[0]
+            valuepath = raw_input("\nEnter the path where the file is located: ")
+            head = os.path.split(valuepath)
+            newhead = head[0]+ head[1]
+            if head[1] == "" :
+                newhead = head[0]
+            elif(head[1][-4:-3] == "."):
+                newhead = head[0]
             if self.verify_path(newhead):
-                file_path=newhead
+                file_path = newhead
                 break
             else: 
                 print "The path does not exist, try again.."
@@ -49,23 +56,29 @@ class SolveConsole():
             valuename = raw_input("\nEnter the file name to\
  read and solve the Sudoku: ")
             if self.verify_file(file_path+"\\"+valuename):
-                file_name=valuename
+                file_name = valuename
                 break
             else:
                 print "The file does not exist, try again.."            
         
         
-        solved = self.Game_console.solve_sudoku_from_file(file_path, file_name)
+        solved = self.game_console.solve_sudoku_from_file(file_path, file_name)
         self.display_or_export_sudoku_solved(solved)
         raw_input("Press any key to continue...")
         
-    def verify_path(self,ispath):
+    def verify_path(self, ispath):
+        '''
+        Verify if the path typed exists
+        '''
         if os.path.exists(ispath):
             return True
         else: 
             return False
         
-    def verify_file(self,isfile):
+    def verify_file(self, isfile):
+        '''
+        Verify if the file typed exists
+        '''
         if os.path.isfile(isfile):
             return True
         else: 
@@ -78,11 +91,11 @@ class SolveConsole():
         print "===================================="    
         print "        Input instructions"
         print "===================================="
-        print("1) Enter 81 numbers and press Enter key.")
-        print("2) Use '0' or '.' to make reference to empty slots.")
-        print("3) Press Enter key, to fill the rest with empty slots")
-        print("4) only numbers are allowed")
-        print("5) To exit this option press Q key\n")
+        print("- Enter 81 numbers and press Enter key.")
+        print("- Use '0' or '.' to make reference to empty slots.")
+        print("- Press Enter key, to fill the rest with empty slots")
+        print("- Only numbers are allowed")
+        print("- To exit, press 'Q' or just 'Enter' key.")
         
         flag = False
         while not flag:
@@ -90,11 +103,11 @@ class SolveConsole():
             if(input_text in "qQ"):
                 flag = False
                 break
-            input_text, flag =self.Game_console.validate_text(input_text)
+            input_text, flag = self.game_console.validate_text(input_text)
             
         if(flag):
             dictionary = input_text
-            solved = self.Game_console.solve_sudoku_from_console(dictionary)
+            solved = self.game_console.solve_sudoku_from_console(dictionary)
             self.display_or_export_sudoku_solved(solved)
             raw_input("Press any key to continue...")
     
@@ -102,9 +115,9 @@ class SolveConsole():
         '''
         Print and/or export the solution
         '''
-        alg =self.Game_console.xml_config_file.\
+        alg = self.game_console.xml_config_file.\
         get_xml_value("default_algorithm")
-        output =self.Game_console.xml_config_file.\
+        output = self.game_console.xml_config_file.\
         get_xml_value("solver_output_type")
         output = output.lower()
             
@@ -113,17 +126,17 @@ class SolveConsole():
         print "================================================"
       
         if (output == 'display by console'):
-            self.Game_console.display(dictionary)
+            self.game_console.display(dictionary)
         if (output == 'export to file'):
-            self.Game_console.display(dictionary)
-            self.Game_console.txtfile.write_file(dictionary)
+            self.game_console.display(dictionary)
+            self.game_console.txtfile.write_file(dictionary)
             print "\n...........The solution was\
  exported to the file...........\n"
             os.system("cls")
                       
     
-    def enter_option(self, length,text,error="Incorrect input!.\
- \nPlease enter a proper option."):
+    def enter_option(self, length, text, error = "Incorrect input!.\
+        \nPlease enter a proper option."):
         while True:
             num = raw_input(text)
             try:
@@ -133,16 +146,11 @@ class SolveConsole():
                     break
                 else:
                     self.error_message(error)
-                    pass
             except ValueError:
                 self.error_message(error)
-                pass
     
     def error_message(self, text):
         '''
         display an error
         '''
         print text
-    
-    def exit_game(self):
-        print "............Exist............\n"
